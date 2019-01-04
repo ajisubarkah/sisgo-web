41153d450dc090cae46fa8539d61508b211d9d92
Mr Plankton <ajisubarkah99@gmail.com>
2019-01-04 12:30:20 +0700

    :memo: Refactor style

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -46,7 +46,7 @@
                         <a class="dropdown-item" href="#">Profile</a>
                         <a class="dropdown-item" href="#">Settings</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="logout">Log out</a>
+                        <a class="dropdown-item" href="{{url('logout')}}">Log out</a>
                     </div>
                 </li>
             </ul>
